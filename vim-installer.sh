a10e3908b7acc3da0c660ee06a31909f9cd753e4
Shreyas <shrykulk.rk85@gmail.com>
2015-07-16 15:27:40 +0000

    Modified vim-installer to get via https:// instead

--- vim-installer.sh
+++ vim-installer.sh
@@ -4,11 +4,11 @@
 mkdir -p ~/.vim/autoload ~/.vim/bundle && curl -LSso ~/.vim/autoload/pathogen.vim https://tpo.pe/pathogen.vim
 
 # Load the necessary .vim bundles
-git clone git@github.com:scrooloose/nerdtree.git ~/.vim/bundle/nerdtree/
-git clone git@github.com:nvie/vim-flake8.git ~/.vim/bundle/vim-flake8/
-git clone git@github.com:jelera/vim-javascript-syntax.git ~/.vim/bundle/vim-javascript-syntax/
-git clone git@github.com:walm/jshint.vim.git ~/.vim/bundle/jshint/	
-git clone git@github.com:tpope/vim-fugitive.git ~/.vim/bundle/vim-fugitive/
+git clone https://github.com/scrooloose/nerdtree.git ~/.vim/bundle/nerdtree/
+git clone https://github.com/nvie/vim-flake8.git ~/.vim/bundle/vim-flake8/
+git clone https://github.com/jelera/vim-javascript-syntax.git ~/.vim/bundle/vim-javascript-syntax/
+git clone https://github.com/walm/jshint.vim.git ~/.vim/bundle/jshint/	
+git clone https://github.com/tpope/vim-fugitive.git ~/.vim/bundle/vim-fugitive/
 
 # Copy the required .vimrc file
 curl -LSSo ~/.vimrc https://raw.githubusercontent.com/shreyasrk/dotfiles/master/.vimrc
